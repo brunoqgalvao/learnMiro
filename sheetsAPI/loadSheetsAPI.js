98baea9357ca1f3a6e4146807f8295eb665a5698
Bruno Galvão <bruno.galvao@nubank.com.br>
2020-09-08 21:42:13 -0300

    testing

--- sheetsAPI/loadSheetsAPI.js
+++ sheetsAPI/loadSheetsAPI.js
@@ -36,4 +36,14 @@ function initClient() {
   }, function(error) {
     appendPre(JSON.stringify(error, null, 2));
   });
+}      /**
+* Append a pre element to the body containing the given message
+* as its text node. Used to display the results of the API call.
+*
+* @param {string} message Text to be placed in pre element.
+*/
+function appendPre(message) {
+ var pre = document.getElementById('content');
+ var textContent = document.createTextNode(message + '\n');
+ pre.appendChild(textContent);
 }
